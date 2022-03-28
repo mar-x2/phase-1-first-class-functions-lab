394e4f9ce86821135d15a2ebc3c92c9a4c2998ce
Maria Marquez <406rem33@gmail.com>
2022-03-27 21:00:13 -0700

    Half assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers = ['Antonia', 'Nuru'];
+};
+    
+    function returnLastTwoDrivers(drivers) {
+        return drivers = ['Amari', 'Mo'];
+    }
+    
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+    
+    
+    const createFareMultiplier = function(num) {
+        return function(multiplier) {
+            return num * multiplier;
+        }
+    }
+    
+    const fareDoubler = function(createFareMultiplier) {
+        return createFareMultiplier * 2;
+        }
+    
+    const fareTripler = function(createFareMultiplier) {
+        return createFareMultiplier * 3;
+    }
+    
